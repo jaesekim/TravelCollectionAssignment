e0b42557d68671828c4323da154ad4220cfdf28c
jaesekim <jaeseki@gmail.com>
2024-01-09 20:27:09 +0900

    Initial Commit

---
+++ TravelCollectionAssignment/TravelRecomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TravelRecomCollectionViewCell.swift
+//  TravelCollectionAssignment
+//
+//  Created by 김재석 on 1/9/24.
+//
+
+import UIKit
+
+class TravelRecomCollectionViewCell: UICollectionViewCell {
+    
+}
